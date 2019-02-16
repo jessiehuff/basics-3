1fc98d0bee8eaf4383794ead6a721a094fa0b838
Jessie Huff <huffjd1@gmail.com>
2019-02-16 18:45:38 -0500

    Practiced using directives with ngFor, ngStyle, and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome!';
+  displayed = false; 
+  allClicks = []; 
+  count = 0; 
+
+  isDisplayed() {
+    this.displayed = !this.displayed
+    this.count = this.count += 1
+    this.allClicks.push(this.count)
+  }
+
+  getColor(){
+    return (this.count > 5) ? 'blue' : 'clear'
+  }
 }
